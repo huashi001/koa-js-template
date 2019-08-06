665a72201e8479050204b0c89c80cdc46ba74163
韩春华 <hanchunhua@corp-ci.com>
2019-08-06 18:02:06 +0800

    1

--- controller/newman.js
+++ controller/newman.js
@@ -1,32 +1,67 @@
-const axios = require('axios');
-class newmanController {
-  async newmanRun(ctx){
-    console.log(axios)
-    ctx.body = 'i am route from home444'
-  }
-  async translate(){
-    axios.get('http://fy.iciba.com/ajax.php',{
-      headers: {
-        host: 'fy.iciba.com',
-        refer: 'http://fy.iciba.com'
-      },
-      params: {
-        a: "fy",
-        f: 'zh-CN',
-        t: 'en',
-        w: '矿泉水'
+
+import { run } from '../services/newman'
+
+class NewmanControllers {
+  // 执行单个request
+  async run (ctx) {
+    const request = ctx.request.body
+    if (!request) {
+      ctx.body = {
+        state: 0,
+        msg: 'no request data'
       }
-    }).then(res => {
-      console.log(res, 'res')
-    }).catch(e =>{
-      console.log(e)
-    })
+      return
+    }
+    try {
+      let summary = await run({
+        "collection": {
+          "item": request
+        }
+      })
+      let res = summary.run.executions[0].response
+      let resBody = summary.run.executions[0].response.stream.toString('utf-8')
+      ctx.body = {
+        state: 1,
+        data: summary,
+        //response: res,
+        test: resBody
+      }
+    } catch (e) {
+      console.log('error', e)
+      ctx.body = {
+        state: 0,
+        msg: `${e}`
+      }
+    }
+  }
+  // 执行collection
+  async runCollection (ctx) {
+    const collection = ctx.request.body
+    if(!collection){
+      ctx.body = {
+        state: 0,
+        msg: 'collection is required'
+      }
+      return
+    }
+    try{
+      let summary = await run({
+        "collection": collection
+      })
+      let executions = summary.run.executions
+      ctx.body = {
+        state: 1,
+        data: summary,
+        executions
+      }
+    }catch(e){
+      console.log('error', e)
+      ctx.body = {
+        state: 0,
+        msg: `${e}`
+      }
+    }
   }
 }
-// module.exports = {
-//   async newmanRun(ctx){
-//     ctx.body = 'i am route from home 2222'
-//   }
-// }
 
-module.exports = new newmanController()
+export default new NewmanControllers();
