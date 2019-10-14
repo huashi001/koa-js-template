652a297b7f42ae67826f4396d27db4361ca48b06
韩春华 <hanchunhua@corp-ci.com>
2019-10-14 19:44:12 +0800

    esonTrans

--- controller/translate.js
+++ controller/translate.js
@@ -56,6 +56,42 @@ class TranslateController {
       data: data
     }
   }
+  
+  // 翻译eson数组
+  async esonTrans (ctx) {
+    let arr = ctx.request.body.data
+    for (let j = 0; j < arr.length; j++) {
+      if (!arr[j].startsWith('#')) {
+        let _temp = arr[j].split(':')
+        if (!_temp[1]) {
+          return
+        }
+        let result = await axios.get('http://fy.iciba.com/ajax.php',{
+          headers: {
+            host: 'fy.iciba.com',
+            refer: 'http://fy.iciba.com'
+          },
+          params: {
+            a: "fy",
+            f: 'zh-CN',
+            t: 'en',
+            w: _temp[1]
+          }
+        })
+        console.log(result.status, result.data)
+        if (result.status === 200 && result.data.status === 1) {
+          _temp[1] = result.data.content.out
+        } else {
+          _temp[1] = '未知错误，请重试'
+        }
+        arr[j] = _temp.join(':')
+      }
+    }
+    ctx.body = {
+      status: 1,
+      data: arr
+    }
+  }
 }
 
 module.exports = new TranslateController()
--- routers/trans.js
+++ routers/trans.js
@@ -4,4 +4,5 @@ let trans = require('../controller/translate')
 let transRouter = new Router()
 transRouter.get('/', trans.translate)
 transRouter.post('/data', trans.batchTrans)
+transRouter.post('/eson', trans.esonTrans)
 module.exports = transRouter
